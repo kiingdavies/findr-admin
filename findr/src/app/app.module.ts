ca064f7d968edf77b77ea9216a797159f7c98d56
kiingdavies <daviesadedeji007@gmail.com>
2020-11-25 16:01:36 +0100

    Initial commit

---
+++ findr/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { AdminTableComponent } from './admin-table/admin-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLoginComponent,
+    AdminTableComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

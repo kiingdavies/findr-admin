66d2cfd352b8c2a0554edf98ec01dc5394e8f41c
kiingdavies <daviesadedeji007@gmail.com>
2020-11-26 15:24:41 +0100

    Logout show when logged in Done

--- findr/src/app/app.component.ts
+++ findr/src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'findr';
 
-  showDiv = {
-    previous : false,
-    current : false,
-    next : false
-  };
+  // showDiv = {
+  //   previous : false,
+  //   current : false,
+  //   next : false
+  // };
 
+  public show: boolean = false;
+  public buttonName: any = 'Show';
+
+  toogle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if (this.show) this.buttonName = 'Logout';
+    else this.buttonName = 'Login';
+  }
 }
--- findr/src/app/app.module.ts
+++ findr/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AdminLoginComponent } from './admin-login/admin-login.component';
+// import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminTableComponent } from './admin-table/admin-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent,
+    // AdminLoginComponent,
     AdminTableComponent
   ],
   imports: [
